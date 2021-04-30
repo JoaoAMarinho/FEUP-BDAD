2f1b5e2509ff61e55c7375ea4e43f669e0706b0d
JoaoMarinho <joao.andre.marinho@gmail.com>
2021-04-30 21:25:21 +0100

    Added tp ex and project files

--- Project/criar.sql
+++ Project/criar.sql
@@ -1,6 +1,25 @@
 PRAGMA foreign_keys=ON;
-BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS VehicleEmployee;
+DROP TABLE IF EXISTS RestaurantFoodItem;
+DROP TABLE IF EXISTS RequestFoodItem;
+DROP TABLE IF EXISTS RestaurantPeriod;
+DROP TABLE IF EXISTS Menu;
+DROP TABLE IF EXISTS FoodItemIngredient;
+DROP TABLE IF EXISTS Owns;
+DROP TABLE IF EXISTS Delivery;
+DROP TABLE IF EXISTS County;
+DROP TABLE IF EXISTS VehicleType;
+DROP TABLE IF EXISTS Vehicle;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Request;
+DROP TABLE IF EXISTS FoodItem;
+DROP TABLE IF EXISTS FoodType;
+DROP TABLE IF EXISTS Ingredient;
+DROP TABLE IF EXISTS FoodItemType;
+DROP TABLE IF EXISTS Period;
+DROP TABLE IF EXISTS Restaurant;
+DROP TABLE IF EXISTS Card;
 DROP TABLE IF EXISTS Client;
 
 CREATE TABLE Client ( 
@@ -13,27 +32,24 @@ CREATE TABLE Client (
     password TEXT,
     CONSTRAINT clientPK PRIMARY KEY (id), 
     CONSTRAINT phoneLength CHECK (phone BETWEEN 900000000 AND 999999999 ),
-    CONSTRAINT nifLength CHECK (LENGTH (nif) == 9),
-    CONSTRAINT contactNotAvailable CHECK ( email NOT NULL OR phone NOT NULL ),
-    CONSTRAINT addressNotNull CHECK ( address NOT NULL ),
-    CONSTRAINT nameNotNull CHECK ( name NOT NULL ),
-    CONSTRAINT passRestrictions CHECK ( password NOT NULL AND LENGTH(password) > 5 ),
+    CONSTRAINT nifLength CHECK (LENGTH (nif) = 9),
+    CONSTRAINT contactNotAvailable CHECK ( email IS NOT NULL OR phone IS NOT NULL ),
+    CONSTRAINT addressNotNull CHECK ( address IS NOT NULL ),
+    CONSTRAINT nameNotNull CHECK ( name IS NOT NULL ),
+    CONSTRAINT passRestrictions CHECK ( password IS NOT NULL AND LENGTH(password) > 5 ),
     CONSTRAINT emailPattern CHECK (email LIKE '%@%')
 );
 
-DROP TABLE IF EXISTS Card;
 
 CREATE TABLE Card ( 
     number INTEGER,
     expirationDate DATE,
     CONSTRAINT cardPK PRIMARY KEY (number),
-    CONSTRAINT cardNumberLength CHECK (LENGTH (number) == 12),
-    CONSTRAINT dateNotNull CHECK ( expirationDate NOT NULL ),
+    CONSTRAINT cardNumberLength CHECK (LENGTH (number) = 12),
+    CONSTRAINT dateNotNull CHECK ( expirationDate IS NOT NULL ),
     CONSTRAINT datePattern CHECK (expirationDate LIKE '__/__')
 );
 
-
-DROP TABLE IF EXISTS Restaurant;
 
 CREATE TABLE Restaurant (
     id INTEGER,
@@ -42,11 +58,11 @@ CREATE TABLE Restaurant (
     evaluation REAL DEFAULT 0.0,
     CONSTRAINT restaurantPK PRIMARY KEY (id),
     CONSTRAINT evaluationValues CHECK (evaluation BETWEEN 0.0 AND 5.0 ),
-    CONSTRAINT nameNotNull CHECK ( name NOT NULL ),
-    CONSTRAINT addressNotNull CHECK ( address NOT NULL )
+    CONSTRAINT nameNotNull CHECK ( name IS NOT NULL ),
+    CONSTRAINT addressNotNull CHECK ( address IS NOT NULL )
 );
 
-DROP TABLE IF EXISTS Period;
+
 
 CREATE TABLE Period ( 
     openTime DATE,
@@ -55,12 +71,11 @@ CREATE TABLE Period (
     CONSTRAINT periodPK PRIMARY KEY (openTime, closeTime, dayOfTheWeek),
     CONSTRAINT dayWeekRange CHECK ( dayOfTheWeek BETWEEN 1 AND 7 ),
     CONSTRAINT validTimeWindow CHECK ( openTime < closeTime ),
-    CONSTRAINT openTimeNotNull CHECK (openTime NOT NULL),
-    CONSTRAINT closeTimeNotNull CHECK (closeTime NOT NULL),
-    CONSTRAINT dayOfTheWeekNotNull CHECK (dayOfTheWeek NOT NULL)
+    CONSTRAINT openTimeNotNull CHECK (openTime IS NOT NULL),
+    CONSTRAINT closeTimeNotNull CHECK (closeTime IS NOT NULL),
+    CONSTRAINT dayOfTheWeekNotNull CHECK (dayOfTheWeek IS NOT NULL)
 );
 
-DROP TABLE IF EXISTS FoodItemType;
 
 CREATE TABLE FoodItemType ( 
     id INTEGER,
@@ -68,18 +83,16 @@ CREATE TABLE FoodItemType (
     CONSTRAINT foodItemTypePK PRIMARY KEY (id)
 ); 
 
-DROP TABLE IF EXISTS Ingredient;
 
 CREATE TABLE Ingredient ( 
     id INTEGER,
     name TEXT UNIQUE,
     calories REAL,
     CONSTRAINT ingredientPK PRIMARY KEY (id),
-    CONSTRAINT nameNotNull CHECK ( name NOT NULL ),
+    CONSTRAINT nameNotNull CHECK ( name IS NOT NULL ),
     CONSTRAINT calorieRange CHECK ( calories > 0.0 )
 );
 
-DROP TABLE IF EXISTS FoodType;
 
 CREATE TABLE FoodType ( 
     id INTEGER,
@@ -87,7 +100,6 @@ CREATE TABLE FoodType (
     CONSTRAINT foodTypePK PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS FoodItem;
 
 CREATE TABLE FoodItem (
     id INTEGER,
@@ -99,16 +111,14 @@ CREATE TABLE FoodItem (
     CONSTRAINT foodItemPK PRIMARY KEY (id),
     CONSTRAINT foodItemTypeFK FOREIGN KEY (foodItemTypeID) REFERENCES FoodItemType(id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT foodTypeFK FOREIGN KEY (foodTypeID) REFERENCES FoodType(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT nameNotNull CHECK ( name NOT NULL ),
-    CONSTRAINT priceNotNull CHECK ( price NOT NULL ),
-    CONSTRAINT foodItemTypeNotNull CHECK ( foodItemTypeID NOT NULL ),
-    CONSTRAINT foodTypeNotNull CHECK ( foodTypeID NOT NULL ),
+    CONSTRAINT nameNotNull CHECK ( name IS NOT NULL ),
+    CONSTRAINT priceNotNull CHECK ( price IS NOT NULL ),
+    CONSTRAINT foodItemTypeNotNull CHECK ( foodItemTypeID IS NOT NULL ),
+    CONSTRAINT foodTypeNotNull CHECK ( foodTypeID IS NOT NULL ),
     CONSTRAINT priceRange CHECK ( price > 0.0 ),
     CONSTRAINT imagePattern CHECK (image LIKE '%.png' OR image LIKE '%.jpeg')
 );
 
-
-DROP TABLE IF EXISTS Request;
 
 CREATE TABLE Request (
     code INTEGER,
@@ -122,19 +132,18 @@ CREATE TABLE Request (
     CONSTRAINT clientFK FOREIGN KEY (clientID) REFERENCES Client(id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT cardFK FOREIGN KEY (cardNumber) REFERENCES Card(number) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT restaurantFK FOREIGN KEY (restaurantID) REFERENCES Restaurant(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT cardNumberLength CHECK (LENGTH (cardNumber) == 12),
-    CONSTRAINT dateNotNull CHECK ( data NOT NULL ),
-    CONSTRAINT timeNotNull CHECK ( time NOT NULL ),
-    CONSTRAINT priceNotNull CHECK ( price NOT NULL ),
-    CONSTRAINT clientNotNull CHECK ( clientID NOT NULL ),
-    CONSTRAINT cardNotNull CHECK ( cardNumber NOT NULL ),
-    CONSTRAINT restaurantNotNull CHECK ( restaurantID NOT NULL ),
+    CONSTRAINT cardNumberLength CHECK (LENGTH (cardNumber) = 12),
+    CONSTRAINT dateNotNull CHECK ( data IS NOT NULL ),
+    CONSTRAINT timeNotNull CHECK ( time IS NOT NULL ),
+    CONSTRAINT priceNotNull CHECK ( price IS NOT NULL ),
+    CONSTRAINT clientNotNull CHECK ( clientID IS NOT NULL ),
+    CONSTRAINT cardNotNull CHECK ( cardNumber IS NOT NULL ),
+    CONSTRAINT restaurantNotNull CHECK ( restaurantID IS NOT NULL ),
     CONSTRAINT priceRange CHECK ( price > 0.0 ),
     CONSTRAINT datePattern CHECK (data LIKE '__/__/____'),
     CONSTRAINT timePattern CHECK (time LIKE '__:__')
 );
 
-DROP TABLE IF EXISTS Employee;
 
 CREATE TABLE Employee ( 
     id INTEGER,
@@ -149,17 +158,16 @@ CREATE TABLE Employee (
     CONSTRAINT employeePK PRIMARY KEY (id),
     CONSTRAINT countyFK FOREIGN KEY (postalCode) REFERENCES County(postalCode) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT phoneLength CHECK (phone BETWEEN 900000000 AND 999999999 ),
-    CONSTRAINT nifLength CHECK (LENGTH (nif) == 9),
-    CONSTRAINT contactNotAvailable CHECK ( email NOT NULL OR phone NOT NULL ),
-    CONSTRAINT addressNotNull CHECK ( address NOT NULL ),
-    CONSTRAINT nameNotNull CHECK ( name NOT NULL ),
-    CONSTRAINT passRestrictions CHECK ( password NOT NULL AND LENGTH(password) > 5 ),
-    CONSTRAINT nifNotNull CHECK ( nif NOT NULL ),
+    CONSTRAINT nifLength CHECK (LENGTH (nif) = 9),
+    CONSTRAINT contactNotAvailable CHECK ( email IS NOT NULL OR phone IS NOT NULL ),
+    CONSTRAINT addressNotNull CHECK ( address IS NOT NULL ),
+    CONSTRAINT nameNotNull CHECK ( name IS NOT NULL ),
+    CONSTRAINT passRestrictions CHECK ( password IS NOT NULL AND LENGTH(password) > 5 ),
+    CONSTRAINT nifNotNull CHECK ( nif IS NOT NULL ),
     CONSTRAINT evaluationValues CHECK (evaluation BETWEEN 0.0 AND 5.0 ),
     CONSTRAINT emailPattern CHECK (email LIKE '%@%')
 );
 
-DROP TABLE IF EXISTS Vehicle;
 
 CREATE TABLE Vehicle ( 
     id INTEGER,
@@ -167,31 +175,30 @@ CREATE TABLE Vehicle (
     vehicleTypeID INTEGER,
     CONSTRAINT vehiclePK PRIMARY KEY (id),
     CONSTRAINT vehicleTypeFK FOREIGN KEY (vehicleTypeID) REFERENCES VehicleType(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT registrationNotNull CHECK ( registration NOT NULL ),
-    CONSTRAINT vehicleTypeNotNull CHECK ( vehicleTypeID NOT NULL ),
+    CONSTRAINT registrationNotNull CHECK ( registration IS NOT NULL ),
+    CONSTRAINT vehicleTypeNotNull CHECK ( vehicleTypeID IS NOT NULL ),
     CONSTRAINT registrationPattern CHECK (registration LIKE '__-__-__')
     
 );
 
-DROP TABLE IF EXISTS VehicleType;
+
 
 CREATE TABLE VehicleType ( 
     id INTEGER,
     name TEXT UNIQUE,
     CONSTRAINT vehicleTypePK PRIMARY KEY (id),
-    CONSTRAINT nameNotNull CHECK ( name NOT NULL )
+    CONSTRAINT nameNotNull CHECK ( name IS NOT NULL )
 );
 
-DROP TABLE IF EXISTS County;
 
 CREATE TABLE County ( 
     name TEXT,
     postalCode INTEGER,
     CONSTRAINT countyPK PRIMARY KEY (postalCode),
-    CONSTRAINT postalCodeLenght CHECK (LENGTH (postalCode) == 4)
+    CONSTRAINT postalCodeLenght CHECK (LENGTH (postalCode) = 4)
 ) WITHOUT ROWID;
 
-DROP TABLE IF EXISTS Delivery;
+
 
 CREATE TABLE Delivery (
     code INTEGER,
@@ -206,10 +213,10 @@ CREATE TABLE Delivery (
     CONSTRAINT vehicleFK FOREIGN KEY (registrationID) REFERENCES Vehicle(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT employeeFK FOREIGN KEY (employeeID) REFERENCES Employee(id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT requestFK FOREIGN KEY (code) REFERENCES Request(code) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    CONSTRAINT arrivalTimeNotNull CHECK ( arrivalTime NOT NULL ),
-    CONSTRAINT priceNotNull CHECK ( price NOT NULL ),
-    CONSTRAINT registrationNotNull CHECK ( registrationID NOT NULL ),
-    CONSTRAINT employeeNotNull CHECK ( employeeID NOT NULL ),
+    CONSTRAINT arrivalTimeNotNull CHECK ( arrivalTime IS NOT NULL ),
+    CONSTRAINT priceNotNull CHECK ( price IS NOT NULL ),
+    CONSTRAINT registrationNotNull CHECK ( registrationID IS NOT NULL ),
+    CONSTRAINT employeeNotNull CHECK ( employeeID IS NOT NULL ),
     CONSTRAINT validTimeWindow CHECK ( scheduledTime <= arrivalTime ),
     CONSTRAINT priceRange CHECK ( price > 0.0 ),
     CONSTRAINT evaluationValues CHECK (evaluation BETWEEN 0.0 AND 5.0 )
@@ -218,37 +225,39 @@ CREATE TABLE Delivery (
 
 -- MANY TO MANY ASSOCIATIONS
 
-DROP TABLE IF EXISTS Owns;
 
 CREATE TABLE Owns ( 
     clientID INTEGER,
     cardNumber INTEGER,
     CONSTRAINT ownsPK PRIMARY KEY (clientID,cardNumber),
     CONSTRAINT clientFK FOREIGN KEY (clientID) REFERENCES Client(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT cardFK FOREIGN KEY (cardNumber) REFERENCES Card(number) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT cardFK FOREIGN KEY (cardNumber) REFERENCES Card(number) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT cardNumberNotNull CHECK ( cardNumber IS NOT NULL )
 );
 
-DROP TABLE IF EXISTS FoodItemIngredient;
+
 
 CREATE TABLE FoodItemIngredient (
     foodItemID INTEGER,
     ingredientID INTEGER,
     CONSTRAINT foodItemIngredientPK PRIMARY KEY (foodItemID,ingredientID),
     CONSTRAINT foodItemFK FOREIGN KEY (foodItemID) REFERENCES FoodItem(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT ingredientFK FOREIGN KEY (ingredientID) REFERENCES Ingredient(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT ingredientFK FOREIGN KEY (ingredientID) REFERENCES Ingredient(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ingredientIDNotNull CHECK ( ingredientID IS NOT NULL )
 );
 
-DROP TABLE IF EXISTS Menu;
+
 
 CREATE TABLE Menu (  
     foodItemID1 INTEGER,
     foodItemID2 INTEGER,
     CONSTRAINT menuPK PRIMARY KEY (foodItemID1, foodItemID2),
     CONSTRAINT foodItemFK1 FOREIGN KEY (foodItemID1) REFERENCES FoodItem(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT foodItemFK2 FOREIGN KEY (foodItemID2) REFERENCES FoodItem(id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT foodItemFK2 FOREIGN KEY (foodItemID2) REFERENCES FoodItem(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT foodItemIDNotNull CHECK ( foodItemID2 IS NOT NULL )
 );
 
-DROP TABLE IF EXISTS RestaurantPeriod;
+
 
 CREATE TABLE RestaurantPeriod (
     restaurantID INTEGER,
@@ -260,7 +269,6 @@ CREATE TABLE RestaurantPeriod (
     CONSTRAINT periodFK FOREIGN KEY (openTime,closeTime,dayOfTheWeek) REFERENCES Period(openTime,closeTime,dayOfTheWeek) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS RequestFoodItem;
 
 CREATE TABLE RequestFoodItem ( 
     code INTEGER,
@@ -269,18 +277,27 @@ CREATE TABLE RequestFoodItem (
     CONSTRAINT requestFoodItemPK PRIMARY KEY(code, foodItemID),
     CONSTRAINT requestFK FOREIGN KEY (code) REFERENCES Request(code) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT foodItemFK FOREIGN KEY (foodItemID) REFERENCES FoodItem(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT foodItemQuantity CHECK ( numberOfFoodItems > 0 )
+    CONSTRAINT foodItemQuantity CHECK ( numberOfFoodItems > 0 ),
+    CONSTRAINT foodItemIDNotNull CHECK ( foodItemID IS NOT NULL )
 
 );
 
-DROP TABLE IF EXISTS VehicleEmployee;
+CREATE TABLE RestaurantFoodItem ( 
+    restaurantID INTEGER,
+    foodItemID INTEGER,
+    CONSTRAINT restaurantFoodItemPK PRIMARY KEY(restaurantID, foodItemID),
+    CONSTRAINT restaurantFK FOREIGN KEY (restaurantID) REFERENCES Restaurant(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT foodItemFK FOREIGN KEY (foodItemID) REFERENCES FoodItem(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT foodItemIDNotNull CHECK ( foodItemID IS NOT NULL )
+
+);
 
 CREATE TABLE VehicleEmployee (
     vehicleID TEXT,
     employeeID TEXT,
     CONSTRAINT vehicleEmployeePK PRIMARY KEY (vehicleID, employeeID),
     CONSTRAINT vehicleFK FOREIGN KEY (vehicleID) REFERENCES Vehicle(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT employeeFK FOREIGN KEY (employeeID) REFERENCES Employee(id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT employeeFK FOREIGN KEY (employeeID) REFERENCES Employee(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT vehicleIDNotNull CHECK ( vehicleID IS NOT NULL ),
+    CONSTRAINT employeeIDNotNull CHECK ( employeeID IS NOT NULL )
 );
-
-COMMIT;
